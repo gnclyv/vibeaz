cc00e036421d99495f840faf08fde8208d2e782d
gnclyv <nihadgancaliyev@gmail.com>
2026-02-02 22:35:39 +0400

    login.js

--- login.js
+++ login.js
@@ -1,1 +1,34 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import { getAuth, RecaptchaVerifier, signInWithPhoneNumber } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
 
+const firebaseConfig = {
+    apiKey: "AIzaSyCUXJcQt0zkmQUul53VzgZOnX9UqvXKz3w",
+    authDomain: "vibeaz-1e98a.firebaseapp.com",
+    projectId: "vibeaz-1e98a",
+    storageBucket: "vibeaz-1e98a.firebasestorage.app",
+    messagingSenderId: "953434260285",
+    appId: "1:953434260285:web:6263b4372487ba6d673b54"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+window.recaptchaVerifier = new RecaptchaVerifier('recaptcha-container', { 'size': 'invisible' }, auth);
+
+document.getElementById('send-sms-btn').onclick = () => {
+    const number = document.getElementById('phoneNumber').value;
+    signInWithPhoneNumber(auth, number, window.recaptchaVerifier)
+        .then(res => {
+            window.confirmationResult = res;
+            document.getElementById('login-step-1').classList.add('hidden');
+            document.getElementById('login-step-2').classList.remove('hidden');
+        }).catch(err => alert("Xəta: " + err.message));
+};
+
+document.getElementById('verify-sms-btn').onclick = () => {
+    const code = document.getElementById('smsCode').value;
+    window.confirmationResult.confirm(code).then(() => {
+        // Giriş uğurlu olduqda ana səhifəyə yönləndir
+        window.location.href = "index.html"; 
+    }).catch(() => alert("Kod yanlışdır!"));
+};

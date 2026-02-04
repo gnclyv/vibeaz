da2293c84ffed774ab25792cca9ebe57c459d010
gnclyv <nihadgancaliyev@gmail.com>
2026-02-04 22:13:00 +0400

    firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -3,22 +3,19 @@ importScripts('https://www.gstatic.com/firebasejs/9.15.0/firebase-messaging-comp
 
 firebase.initializeApp({
     apiKey: "AIzaSyCUXJcQt0zkmQUul53VzgZOnX9UqvXKz3w",
-    authDomain: "vibeaz-1e98a.firebaseapp.com",
     projectId: "vibeaz-1e98a",
-    storageBucket: "vibeaz-1e98a.firebasestorage.app",
     messagingSenderId: "953434260285",
     appId: "1:953434260285:web:6263b4372487ba6d673b54"
 });
 
 const messaging = firebase.messaging();
 
-// Arxa fonda bildiriş gələndə nə baş versin
+// Sayt bağlı olanda bildirişi göstər
 messaging.onBackgroundMessage((payload) => {
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
-    body: payload.notification.body,
-    icon: '/icon.png' // Sənin saytın logosu
-  };
-
-  self.registration.showNotification(notificationTitle, notificationOptions);
+    const notificationTitle = payload.notification.title;
+    const notificationOptions = {
+        body: payload.notification.body,
+        icon: '/logo.png' // Varsa loqonuzun yolu
+    };
+    self.registration.showNotification(notificationTitle, notificationOptions);
 });

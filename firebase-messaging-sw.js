19a4e5168f4057c9b0c41a3dad2d0250aab01c01
gnclyv <nihadgancaliyev@gmail.com>
2026-02-04 22:04:00 +0400

    firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -3,21 +3,22 @@ importScripts('https://www.gstatic.com/firebasejs/9.15.0/firebase-messaging-comp
 
 firebase.initializeApp({
     apiKey: "AIzaSyCUXJcQt0zkmQUul53VzgZOnX9UqvXKz3w",
+    authDomain: "vibeaz-1e98a.firebaseapp.com",
     projectId: "vibeaz-1e98a",
+    storageBucket: "vibeaz-1e98a.firebasestorage.app",
     messagingSenderId: "953434260285",
     appId: "1:953434260285:web:6263b4372487ba6d673b54"
 });
 
 const messaging = firebase.messaging();
 
-// Arxa planda mesaj gələndə nə baş versin
+// Arxa fonda bildiriş gələndə nə baş versin
 messaging.onBackgroundMessage((payload) => {
-    console.log('Arxa planda mesaj alındı: ', payload);
-    const notificationTitle = payload.notification.title;
-    const notificationOptions = {
-        body: payload.notification.body,
-        icon: '/vibeaz_logo.png'
-    };
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: '/icon.png' // Sənin saytın logosu
+  };
 
-    self.registration.showNotification(notificationTitle, notificationOptions);
+  self.registration.showNotification(notificationTitle, notificationOptions);
 });
